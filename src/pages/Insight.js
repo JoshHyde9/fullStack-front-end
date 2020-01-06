fc6bc993903712c0e4a76dace029ea4633b5fc52
JoshHyde9 <joshhyde546@gmail.com>
2020-01-06 20:28:54 +1100

    Too hard

--- src/pages/Insight.js
+++ src/pages/Insight.js
@@ -1,6 +1,16 @@
 import React, { Component } from "react";
 
+const API_URL = `https://enigmatic-fjord-24728.herokuapp.com/api/v1/nasa/insight`;
+
 export default class Insight extends Component {
+  async componentDidMount() {
+    await fetch(API_URL)
+      .then(result => result.json())
+      .then(data => {
+        return console.log(data);
+      });
+  }
+
   render() {
     return <div></div>;
   }
